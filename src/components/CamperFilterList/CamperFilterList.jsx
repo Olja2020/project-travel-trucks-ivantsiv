ec21c2da60f9773b560f085bc9671b79f79ed317
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-09-30 00:20:18 +0300

    add

--- src/components/CamperFilterList/CamperFilterList.jsx
+++ src/components/CamperFilterList/CamperFilterList.jsx
@@ -1,105 +1,21 @@
 
-// const CamperFilterList=()=>{
-
-// }
-// export default CamperFilterList;
-
-// import { 
-//     //useEffect, 
-//     useState } from 'react';
-// import { useSelector } from 'react-redux';
-// import Camper from '../Camper/Camper';
-// import css from './CamperFilterList.module.css';
-// import {
-//   selectError,
-//   selectIsLoading,
-  
-// } from '../../redux/campers/selectors';
-// import { InfinitySpin } from 'react-loader-spinner';
-// import {
- 
-//   selectCampersFilter
-// } from '../../redux/filters/selectors';
-
-
-// const CamperFilterList = () => {
-
-// //   const [sortType, setSortType]= useStare(0);
-// //   const [sortName, setSortName]=useState(0);
-// //   useEffect (()=> {
-// //     fetch("")
-// //     .then((res)=> res.json())
-// //     .then((arr)=>{
-      
-// //     })
-// //   })
-//   const isLoading = useSelector(selectIsLoading);
-//   const error = useSelector(selectError);
-//   const campersFilter = useSelector(selectCampersFilter);
-
-//   // Локальное состояние для отслеживания количества отображаемых элементов
-//   const [visibleCampers, setVisibleCampers] = useState(4); // Начальное количество элементов
-
-//   // Обработчик для кнопки "Load More"
-//   const handleLoadMore = () => {
-//     setVisibleCampers((prevVisibleCampers) => prevVisibleCampers + 4); // Увеличиваем на 5 элементов
-//   };
-
-//   return (
-//     <div>
-//       {isLoading && !error && (
-//         <ClipLoader
-//           visible={true}
-//           width="200"
-//           color="#FFC531"
-//           ariaLabel="infinity-spin-loading"
-//         />
-//       )}
-
-//       <ul className={css.camperList}>
-//         {campersFilter.slice(0, visibleCampers).map((camper) => (
-//           <li key={camper.id} className={css.camperItem}>
-//             <Camper camper={camper} />
-//           </li>
-//         ))}
-//       </ul>
-
-//       {/* Если есть ещё элементы для отображения, показываем кнопку "Load More" */}
-//       {visibleCampers < campersFilter.length && (
-//         <button onClick={handleLoadMore} className={css.loadMoreBtn}>
-//           Load More
-//         </button>
-//       )}
-
-//       {error && <p className={css.error}>Error loading campers: {error}</p>}
-//     </div>
-//   );
-// };
-
-// export default CamperFilterList;
-
-//import { selectFilters } from '../../redux/filters/slice';
+/
 
 import { useSelector, useDispatch } from 'react-redux';
      import { Suspense, useEffect, } from 'react'; 
      import Camper from '../Camper/Camper'; // Оголошення Camper як lazy
      import css from './CamperFilterList.module.css';
      import { selectError, selectIsLoading } from '../../redux/campers/selectors';
-     //import { ClipLoader } from 'react-spinners';
-     //import { selectCampersFilter } from '../../redux/filters/selectors';
+     
      import { fetchCampers } from '../../redux/campers/slice'; 
-     //import 
-     //React,
-      //{ createContext, useContext, useState } from 'react';
-
-     //import axios from 'axios';
+     
       const CamperFilterList = () => {
       
        const dispatch = useDispatch();
-       //const campers = useSelector(selectCampers);
+       
        const isLoading = useSelector(selectIsLoading);
        const error = useSelector(selectError);
-       //const filteredCampers = useSelector(selectCampersFilter); 
+       
 
 //+=+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //console.log(selectFilters)
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -5,11 +5,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { lazy, useEffect, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-// import { refreshUser } from "./redux/auth/operations";
-// import { selectIsRefreshing } from "./redux/auth/selectors";
+
 
 import { Toaster } from 'react-hot-toast';
-import { InfinitySpin } from 'react-loader-spinner';
+import { ClipLoader } from 'react-spinner';
 
 const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
 const NotFoundPage = lazy(() =>
@@ -19,24 +18,11 @@ const CatalogPage = lazy(() => import('../../pages/CatalogPage/CatalogPage'));
 const CamperDetailsPage = lazy(() => import('../../pages/CamperDetailsPage/CamperDetailsPage'));
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  // const isRefreshing = useSelector(selectIsRefreshing);
-
-  // useEffect(() => {
-  //   dispatch(refreshUser());
-  // }, [dispatch]);
+   
 
   return (
     <>
-      {/* <Suspense fallback={<div className="loader">
-      <InfinitySpin
-        visible={true}
-        width="200"
-        color="#FFC531"
-        ariaLabel="infinity-spin-loading"
-      />
-    </div>}> */}
+     
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
@@ -45,7 +31,7 @@ export default function App() {
             <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
-      {/* </Suspense> */}
+      
       <Toaster position="top-center" reverseOrder={false} />
     </>
   );
